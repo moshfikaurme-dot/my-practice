382eb641ade43f2a52733da1444860f0fa6a40a9
Moshfika Najnin <moshfikaurme@gmail.com>
2025-12-29 20:46:29 +0600

    Add simple C++ program to display basic information and hello world

---
+++ Ad1_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int i;
+    cin>>i;
+    std:: cout << "hello world"<<endl;
+    cout << "name : moshfika"<< endl;
+    cout << "age:20";
+
+    return 0;
+}
