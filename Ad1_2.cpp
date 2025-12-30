6dc3ab4c21b476196fdb7e5d41d9784205e7ef26
Moshfika Najnin <moshfikaurme@gmail.com>
2025-12-30 21:06:15 +0600

    Add C++ program to print name and age using basic input/output

---
+++ Ad1_2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+     
+    int i;
+    int age;
+    string name = "moshfika";
+    cin >>i;
+    std :: cout << "name "<< name<<endl;
+    cout << "age "<<age<<endl;
+
+    return 0;
+}
