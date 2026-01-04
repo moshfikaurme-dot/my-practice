168fce9ac9deacc385f41db7d6b181783d5211ca
Moshfika Najnin <moshfikaurme@gmail.com>
2026-01-04 23:15:13 +0600

    Add C++ program to calculate the area of a triangle using base and height

---
+++ Ad1_4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float b,h,area;
+
+    cout << "input base and hight :"<<endl;
+    cin >> b>>h;
+    area = 0.5*b*h;
+    cout << "area "<< area;
+
+    return 0;
+}
